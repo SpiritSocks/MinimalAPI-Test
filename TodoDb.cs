d666554efeab92fd2394f4ec51dc91317d550d61
Valera <vdteterich@gmail.com>
2025-01-25 01:30:46 +0300

    Add files via upload

---
+++ TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentMinimalAPI.Models;
+
+namespace StudentMinimalAPI {
+    public class TodoDb : DbContext {
+
+        public DbSet<Todo> Todos => Set<Todo>();
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+        
+    }
+}
